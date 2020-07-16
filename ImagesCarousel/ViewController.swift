9099472f75b7e4f3e5f7fe41b3ea291fc196e4c4
maikytech <maikytech1307@gmail.com>
2020-07-16 17:41:05 -0500

    Project Start

---
+++ ImagesCarousel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImagesCarousel
+//
+//  Created by Maiqui Cedeño on 16/07/20.
+//  Copyright © 2020 Poseto Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
